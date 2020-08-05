57086c6b5dba7a90686bfa5c60cdba043c840310
JanisDeveloper <janis@developer.lv>
2020-08-06 00:22:25 +0300

    Updates

---
+++ precache-manifest.c5ec3d2fe94df5fda6ac36958d1bdadb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554f358bdfeff1d2a0559959c83bb4cb",
+    "url": "/react-profile-test-app/index.html"
+  },
+  {
+    "revision": "14a46cb741fd6f3488b7",
+    "url": "/react-profile-test-app/static/css/main.24651e63.chunk.css"
+  },
+  {
+    "revision": "97fb8396d35298be82b2",
+    "url": "/react-profile-test-app/static/js/2.69fc1bd3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-profile-test-app/static/js/2.69fc1bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a46cb741fd6f3488b7",
+    "url": "/react-profile-test-app/static/js/main.afe5933a.chunk.js"
+  },
+  {
+    "revision": "1c17e5b3a71140be27f7",
+    "url": "/react-profile-test-app/static/js/runtime-main.77e3aa73.js"
+  }
+]);
